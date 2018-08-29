b892985a5676cf58c4659658e22ba95f0cc92531
Nguyen Hai <nguyentrihai93@gmail.com>
2018-08-29 22:03:09 +0900

    switch documentation job to new PTI
    This is a mechanically generated patch to switch the documentation
    jobs to use the new PTI versions of the jobs as part of the
    python3-first goal.
    
    See the python3-first goal document for details:
    https://governance.openstack.org/tc/goals/stein/python3-first.html
    
    Change-Id: I766b9d504ea24e1fadaad0d158da43012a07290a
    Story: #2002586
    Task: #24328

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,7 +29,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'rst2pdf.pdfbuilder',
     'sphinx.ext.autodoc',
     'sphinxcontrib.httpdomain',
     'openstackdocstheme',
